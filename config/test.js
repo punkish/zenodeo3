56abe691700d86232c875527a0b93da5e99a9e9c
punkish <punk.kish@gmail.com>
2020-09-11 13:02:19 +0200

    added config/test

---
+++ config/test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/**************************************************************
+ * 
+ * The config options start with all the values in this file
+ * 
+ **************************************************************/
+
+module.exports = {
+    url: 'http://z3.punkish.org/v3',
+    env: 'test'
+}
