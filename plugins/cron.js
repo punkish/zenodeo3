b1d831d1b9e341bd9ac54796bc58237beaac6a71
punkish <punk.kish@gmail.com>
2022-12-15 10:27:04 +0100

    added more cron jobs for caching at midnight

--- plugins/cron.js
+++ plugins/cron.js
@@ -19,7 +19,59 @@ const options = {
                 }
             },
             start: true
-        }
+        },
+        {
+            // daily at five past midnight UTC
+            cronTime: '5 0 * * *',
+            onTick: async server => {
+                try {
+                    await server.inject('/v3/treatmentimages?family=Formicidae&cols=httpUri&cols=treatmentTitle&cols=zenodoDep&cols=treatmentId&cols=captionText&refreshCache=true');
+                } 
+                catch (err) { 
+                    console.error(err);
+                }
+            },
+            start: true
+        },
+        {
+            // daily at ten past midnight UTC
+            cronTime: '10 0 * * *',
+            onTick: async server => {
+                try {
+                    await server.inject('/v3/treatmentimages?class=Actinopterygii&cols=httpUri&cols=treatmentTitle&cols=zenodoDep&cols=treatmentId&cols=captionText&refreshCache=true');
+                } 
+                catch (err) { 
+                    console.error(err);
+                }
+            },
+            start: true
+        },
+        {
+            // daily at ten past midnight UTC
+            cronTime: '15 0 * * *',
+            onTick: async server => {
+                try {
+                    await server.inject('/v3/treatmentimages?class=Arachnida&cols=httpUri&cols=treatmentTitle&cols=zenodoDep&cols=treatmentId&cols=captionText&refreshCache=true');
+                } 
+                catch (err) { 
+                    console.error(err);
+                }
+            },
+            start: true
+        },
+        {
+            // daily at fifteen past midnight UTC
+            cronTime: '15 0 * * *',
+            onTick: async server => {
+                try {
+                    await server.inject('/v3/treatmentimages?class=Malacostraca&cols=httpUri&cols=treatmentTitle&cols=zenodoDep&cols=treatmentId&cols=captionText&refreshCache=true');
+                } 
+                catch (err) { 
+                    console.error(err);
+                }
+            },
+            start: true
+        },
     ]
 };
 
