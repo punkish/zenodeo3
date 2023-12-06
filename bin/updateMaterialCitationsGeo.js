d15cb4ef3938a118ab901fb1c3873044a0fa0d15
punkish <punk.kish@gmail.com>
2023-12-06 20:36:28 +0100

    added explanatory notes to the script

--- bin/updateMaterialCitationsGeo.js
+++ bin/updateMaterialCitationsGeo.js
@@ -1,3 +1,15 @@
+/*
+ * This script is required to be run only once, to update and existing 
+ * materialCitations with ecoregions_id and biomes_id values from the attached 
+ * geodata.ecoregionsGeopoly table. Once the materialCitations table is 
+ * initialized, a TEMPORARY TRIGGER created by truebug does the same job 
+ * everytime new rows are added to the materialCitations table.
+ * 
+ * Run this script from the app root like so
+ * 
+ * $ ➜  zenodeo3 git:(master) node bin/updateMaterialCitationsGeo.js
+ */
+
 import { initDb } from '../lib/dbconn.js';
 const db = initDb();
 
@@ -35,14 +47,11 @@ function updateMaterialCitationsWithGeo() {
         const row500 = !(count % 500);
         const row5000 = !(count % 5000);
 
-        if (row500) {
-            process.stdout.write('.');
+        if (count > 0) {
+            if (row500) process.stdout.write('.');
+            if (row5000) process.stdout.write(` ${count} `);
         }
-
-        if (row5000) {
-            process.stdout.write(` ${count} `);
-        }
-
+        
         count++;
     }
 }
