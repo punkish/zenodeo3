c739b6813e2da71d8b7735fe74d7339cb6370d1d
punkish <punk.kish@gmail.com>
2023-12-08 17:21:44 +0100

    fixed cron job queries

--- plugins/cron.js
+++ plugins/cron.js
@@ -3,31 +3,48 @@
  * @see https://www.npmjs.com/package/fastify-cron
  */
 
-const cols = 'cols=treatmentId&cols=treatmentTitle&cols=zenodoDep&cols=httpUri&cols=caption';
+const cols = 'page=1&size=30&cols=treatmentId&cols=treatmentTitle&cols=zenodoDep&cols=treatmentDOI&cols=articleTitle&cols=articleAuthor&cols=httpUri&cols=caption';
 
 const queries = {
     images: [
-        'cols=',
-        'family=Formicidae',
-        'class=Actinopterygii',
-        'class=Arachnida',
-        'class=Malacostraca',
-        'captionText=phylogeny',
-        'q=phylogeny',
-        'q=phylogeny AND plantae',
-        'journalTitle=eq(European Journal of Taxonomy)',
-        'articleDOI=10.11646/zootaxa.5284.3.7',
-        'articleTitle=starts_with(Morphology and taxonomic assessment)',
+        `cols=`,
+        `family=Formicidae`,
+        `class=Actinopterygii`,
+        `class=Arachnida`,
+        `class=Malacostraca`,
+        `captionText=phylogeny`,
+        `q=phylogeny`,
+        `q=phylogeny AND plantae`,
+        `journalTitle=eq(European Journal of Taxonomy)`,
+        `articleDOI=10.11646/zootaxa.5284.3.7`,
+        `articleTitle=starts_with(Morphology and taxonomic assessment)`,
         `geolocation=within(radius:10, units:'kilometers', lat:40.21, lng:-120.33)`,
-        'class=Actinopterygii&publicationDate=since(2021-12-21)',
-        'checkinTime=since(yesterday)',
-        'q=tyrannosaurus&authorityName=Osborn',
-        'family=Agamidae',
-        'q=moloch OR horridus',
-        'q=decapoda&journalTitle=not_like(zootaxa)'
+        `class=Actinopterygii&publicationDate=since(2021-12-21)`,
+        `checkinTime=since(yesterday)`,
+        `q=tyrannosaurus&authorityName=Osborn`,
+        `family=Agamidae`,
+        `q=moloch OR horridus`,
+        `q=decapoda&journalTitle=not_like(zootaxa)`,
+        `biome=savanna`,
+        `biome=veld`,
+        `biome=pampas`,
+        `biome=Tundra`,
+        `biome=Tropical and Subtropical Moist Broadleaf Forests`,
+        `biome=Mediterranean Forests, Woodlands and Scrub`,
+        `biome=Deserts and Xeric Shrublands`,
+        `biome=Temperate Grasslands, Savannas and Shrublands`,
+        `biome=Boreal Forests/Taiga`,
+        `biome=Temperate Conifer Forests`,
+        `biome=Temperate Broadleaf and Mixed Forests`,
+        `biome=Montane Grasslands and Shrublands`,
+        `biome=Mangroves`,
+        `biome=Flooded Grasslands and Savannas`,
+        `biome=Tropical and Subtropical Grasslands, Savannas and Shrublands`,
+        `biome=Tropical and Subtropical Dry Broadleaf Forests`,
+        `biome=Tropical and Subtropical Coniferous Forests`
     ],
     treatments: [
-        'cols='
+        `cols=`
     ]
 };
 
@@ -46,8 +63,8 @@ Object.keys(queries)
 
             return {
 
-                // starting at midnight, every two mins
-                cronTime: `${i * 2} 0 * * *`,
+                // starting at midnight, every min
+                cronTime: `${(i * 1)} 0 * * *`,
                 onTick: async server => {
                     try {
                         await server.inject(qs);
