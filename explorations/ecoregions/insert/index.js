55e054f53f8d5ab8d36ddb2d0a1f87f4f3a15bfa
punkish <punk.kish@gmail.com>
2024-05-11 23:22:43 +0200

    cleaned up gitignore files from repo

--- explorations/ecoregions/insert/index.js
+++
@@ -1,5 +0,0 @@
-import * as treatments from './treatments.js';
-import * as materialCitations from './materialCitations.js';
-import * as ecoregions from './ecoregions.js';
-
-export { treatments, materialCitations, ecoregions };
--- explorations/urlparse.js
+++
@@ -1,6 +0,0 @@
-import url from 'node:url';
-import Database from "better-sqlite3";
-const db = new Database(':memory:', { verbose: console.log });
-const str = 'https://podcasts.files.bbci.co.uk/b05qqhqp.rss';
-const myURL = new URL(str); 
-console.log(myURL);
