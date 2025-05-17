f1101e306018cda0a94abc9975c0e2f400336e65
punkish <punk.kish@gmail.com>
2025-05-17 13:28:07 +0200

    upped default cache ttl to 7 days

--- plugins/zcache/index.js
+++ plugins/zcache/index.js
@@ -11,7 +11,7 @@ async function zcache(fastify, options) {
         //dir: './',
         name: 'cache',
         space: options.resourceName || 'default',
-        ttl: config.cache.ttl
+        ttl: 7 * 24 * 60 * 60 * 1000
     });
 
     await cache.init();
