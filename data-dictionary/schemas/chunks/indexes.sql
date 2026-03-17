7273572e462e87f4cfc89ae865edee91e7c85b67
punkish <punk.kish@gmail.com>
2026-03-17 18:26:13 +0100

    updated indexes in schemas

--- data-dictionary/schemas/chunks/indexes.sql
+++ data-dictionary/schemas/chunks/indexes.sql
@@ -1,9 +1,8 @@
 -- Index to quickly find chunks for a treatment
 CREATE INDEX IF NOT EXISTS idx_chunks_treatment ON treatment_chunks(treatments_id);
-
 CREATE INDEX IF NOT EXISTS idx_chunks_treatmentId ON treatment_chunks(treatmentId);
 
 -- For resuming: find un-processed treatments fast
 CREATE INDEX IF NOT EXISTS idx_progress_status ON embedding_progress(status);
-
 CREATE INDEX IF NOT EXISTS idx_vectors_treatment ON chunk_vectors(treatments_id);
+CREATE INDEX IF NOT EXISTS idx_vectors_chunk ON chunk_vectors(chunk_id);
--- data-dictionary/schemas/zenodeo/indexes.sql
+++ data-dictionary/schemas/zenodeo/indexes.sql
@@ -1,1 +1,41 @@
+CREATE INDEX IF NOT EXISTS figureCitations_treatmentsId ON figureCitations (treatments_id);
+CREATE INDEX IF NOT EXISTS materialCitations_treatmentsId ON materialCitations(treatments_id)
+CREATE INDEX IF NOT EXISTS figureCitations_images ON figureCitations(
+    id,
+    treatments_id,
+    httpUri,
+    captionText
+);
+CREATE INDEX IF NOT EXISTS treatments_images ON treatments(
+    id,
+    treatmentId,
+    treatmentTitle,
+    treatmentDOI,
+    zenodoDep,
+    articleTitle,
+    articleAuthor
+);
+CREATE INDEX IF NOT EXISTS materialCitations_images ON materialCitations(
+    treatments_id,
+    latitude,
+    longitude
+);
+CREATE INDEX IF NOT EXISTS treatments_orders ON treatments(orders_id);
+CREATE INDEX IF NOT EXISTS treatments_classes ON treatments(classes_id);
+CREATE INDEX IF NOT EXISTS treatments_families ON treatments(families_id);
+CREATE INDEX IF NOT EXISTS treatments_binomens ON treatments(
+    rank,
+    genera_id,
+    species_id
+);
+CREATE INDEX IF NOT EXISTS treatments_articleDOI ON treatments(articleDOI);
+CREATE INDEX IF NOT EXISTS materialCitations_validGeo ON materialCitations(validGeo);
+CREATE INDEX IF NOT EXISTS treatments_journalYear ON treatments(journalYear);
+CREATE INDEX IF NOT EXISTS treatments_journals_id ON treatments(journals_id);
+CREATE INDEX IF NOT EXISTS treatments_genera_species ON treatments(genera_id, species_id);
+CREATE INDEX IF NOT EXISTS ix_ecoregions_id_ecoregionsPolygons ON ecoregionsPolygons(ecoregions_id);
+CREATE INDEX IF NOT EXISTS treatmentAuthors_idx ON treatmentAuthors(treatmentAuthor);
 CREATE INDEX IF NOT EXISTS rowcounts_tblname ON rowcounts(tblname);
+CREATE INDEX IF NOT EXISTS idx_genera_genus_lower ON genera(Lower(genus));
+CREATE INDEX IF NOT EXISTS idx_species_species_lower ON species(Lower(species));
+CREATE INDEX IF NOT EXISTS idx_ta_treatments_id ON treatmentAuthors(treatments_id);
